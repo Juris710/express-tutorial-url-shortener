2bc724016b79f46beefdae05414b6c098c3f09ae
Juris710 <50994088+Juris710@users.noreply.github.com>
2021-05-29 22:26:36 +0900

    Implement minimal example

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const path = require("path");
+const crypto = require("crypto");
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
+
+app.get("/", (req, res)=>{
+  res.sendFile(path.resolve("index.html"));
+})
+
+let urls = {};
+
+app.post("/", (req, res)=>{
+  const url = req.body.url;
+  const slug = crypto.randomBytes(8).toString('hex');
+  urls[slug] = url;
+  console.log(urls);
+  res.send(`localhost:3000/${slug}`);
+});
+
+app.get("/:slug", (req, res)=>{
+  const slug = req.params.slug;
+  res.redirect(urls[slug]);
+});
+
+app.listen(3000, ()=>{console.log("Listening.")});
